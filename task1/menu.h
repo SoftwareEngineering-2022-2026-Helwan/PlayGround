addfabace895c3bf2e74123c7bbf7372ac9b6eaa
testo <testo@gmail.com>
2024-02-24 03:43:46 +0200

    ready to work!

---
+++ task1/menu.h
@@ -0,0 +1,22 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+#include "./menu.c"
+
+//========( main menu )========
+int taskMenu();
+
+
+//========( calc menu )========
+int calcOptionMenu();
+
+
+//========( unit converter menu )========
+int unitConvOptionMenu();
+
+
+//========( money converter menu )========
+int moneyConvOptionMenu();
+
+
+
+#endif
